0d880bc70259419d0d23ed8d2ad52dc6cdf934d1
mami <matt.mitchell@anevis-solutions.com>
2017-07-07 19:17:25 -0400

    testing youtube api functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+  if (event.data === 0) {
+    $.fancybox.close();
+	$('#bgmusic').prop("muted", false);
+}
+
+function onYouTubePlayerAPIReady(){
+   $(document).ready(function() {
+     $(".fancybox")
+	   .attr('rel', 'gallery')
+	   .fancybox({
+	     openEffect  : 'none',
+		 closeEffect : 'none',
+		 nextEffect  : 'none',
+		 prevEffect  : 'none',
+		 padding     : 0,
+		 margin      : 50,
+		 beforeShow  : function() {
+		   // Find the iframe ID
+		   var id = $.fancybox.inner.find('iframe').attr('id');
+           // Create video player object and add event listeners
+		   var player = new YT.Player(id, {
+		     events: {
+			   'onReady': onPlayerReady,
+			   'onStateChange': onPlayerStateChange
+			 }
+		   });
+		 }
+	   });
+	 });
+}
+
 function main(){
   $('.title').on('click', function(){
     $('.menu').fadeToggle(400);
