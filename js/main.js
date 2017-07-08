9ca982e97f5bc4c47611d6ada58f139883bb461f
mami <matt.mitchell@anevis-solutions.com>
2017-07-08 01:13:32 -0400

    added custom letters page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
+/*function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+  if (event.data === 0) {
+    $.fancybox.close();
+  }
+}
+
+function onYouTubePlayerAPIReady() {
+  $('.youtube').fancybox({
+    beforeShow: function() {
+	  var id = $.fancybox.inner.find('iframe').attr('id');
+	  var player = new YT.Player(id, {
+	    events: {
+		  'onReady': onPlayerReady,
+		  'onStateChange': onPlayerStateChange
+		}
+	  };
+	}
+  }
+}*/
+
 function main(){
+  //onYouTubePlayerAPIReady();
+
   $('.title').on('click', function(){
     $('.menu').fadeToggle(400);
 	//$('#subtitle').toggle(); //Replaces subtitle with menu
